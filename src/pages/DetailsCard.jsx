cbc77f4a27647f5ca850a53144df9f0ebc40bdb7
MartaPanasyuk <mart.panasyuk@gmail.com>
2022-05-27 23:03:42 +0200

    added lists pokemon

--- src/pages/DetailsCard.jsx
+++ src/pages/DetailsCard.jsx
@@ -1,15 +1,36 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { useParams } from "react-router-dom";
 
 export default function DetailsCard() {
   const params = useParams();
+
+  const [pokemon, setPokemon] = useState(null);
+
+  useEffect(() => {
+    const fetchByName = async () => {
+      const pokeName = params.pokeName;
+      try {
+        const response = await axios.get(
+          `https://pokeapi.co/api/v2/pokemon/${pokeName}`
+        );
+        console.log(response);
+      } catch (e) {
+        console.log(e.message);
+      }
+    };
+    fetchByName();
+  }, []);
+
   console.log("what is a params", params);
   return (
     <div>
-      <h3>Details</h3>
       <Link to={"/"}>
         <button>Go Home</button>
       </Link>
+      <div>Pokemon Details:</div>
+      <h3></h3>
     </div>
   );
 }
